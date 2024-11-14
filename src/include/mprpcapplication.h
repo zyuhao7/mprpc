9a589714793b9cf528432b2ea4fc26f14850649c
zyuhao7 <zyuhao7@yeah.net>
2024-11-14 17:12:08 +0800

    include

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "mprpcconfig.h"
 
 // mprpc 框架基础类 负责初始化
 class MprpcApplication
@@ -6,8 +7,10 @@ class MprpcApplication
 public:
     static void Init(int argc, char **argv);
     static MprpcApplication &GetInstance();
+    static MprpcConfig &GetConfig();
 
 private:
+    static MprpcConfig m_config;
     MprpcApplication() {}
     MprpcApplication(const MprpcApplication &) = delete;
     MprpcApplication(MprpcApplication &&) = delete;
--- src/include/rpcprovider.h
+++ src/include/rpcprovider.h
@@ -1,12 +1,30 @@
 #pragma once
+#include <memory>
 #include "google/protobuf/service.h"
+#include <muduo/net/TcpServer.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
+#include <muduo/net/TcpConnection.h>
+#include <muduo/net/Callbacks.h>
+#include <muduo/net/Buffer.h>
+#include <muduo/base/Timestamp.h>
+
 // 框架提供的专门服务发布 rpc 服务的网络对象类
 class RpcProvider
 {
 public:
-    // 框架提供给发布方使用可以发布 rpc 方法的函数接口
+    // 框架提供给发布方使用, 可以发布 rpc 方法的函数接口
     void NotifyService(google::protobuf::Service *service);
 
     // 启动 rpc 服务节点
     void Run();
+
+private:
+    // 组合 EventLoop
+    muduo::net::EventLoop m_eventLoop;
+    // 新的socket 连接回调.
+    void OnConnection(const muduo::net::TcpConnectionPtr &);
+    // 已建立连接的读写事件回调.
+    void OnMessage(const muduo::net::TcpConnectionPtr &, muduo::net::Buffer *buf,
+                   muduo::Timestamp);
 };
