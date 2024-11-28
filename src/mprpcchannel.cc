5f64c7b53d283016c9d05770761d04c1df785075
zyuhao7 <zyuhao7@yeah.net>
2024-11-28 17:43:56 +0800

    rpc 方法处理类 实现

--- src/mprpcchannel.cc
+++ src/mprpcchannel.cc
@@ -1,10 +1,15 @@
 #include "mprpcchannel.h"
 #include "rpcheader.pb.h"
+#include "mprpcapplication.h"
+#include "mprpccontroller.h"
+
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #include <error.h>
-
 /*
     header_size  + service_name  method_name ars_size + args
 */
@@ -14,6 +19,7 @@ void mprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
                               google::protobuf::Message *response,
                               google::protobuf::Closure *done)
 {
+    std::cout << "mprpcChannel::CallMethod" << std::endl;
     const google::protobuf::ServiceDescriptor *sd = method->service();
     std::string service_name = sd->name();
     std::string method_name = method->name();
@@ -27,7 +33,7 @@ void mprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
     }
     else
     {
-        std::cout << "serialize request error!" << std::endl;
+        controller->SetFailed("serialize request error!");
         return;
     }
 
@@ -46,7 +52,7 @@ void mprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
     }
     else
     {
-        std::cout << "serialize rpc header error!" << std::endl;
+        controller->SetFailed("serialize rpc header error!");
         return;
     }
     // 组织待发送的 rpc 请求的字符串
@@ -59,7 +65,7 @@ void mprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
     std::cout << "=====================================" << std::endl;
 
     std::cout << "header_size: " << header_size << std::endl;
-    std::cout << "rpc_header_size: " << rpc_header_str << std::endl;
+    std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
     std::cout << "service_name: " << service_name << std::endl;
     std::cout << "method_name: " << method_name << std::endl;
     std::cout << "args_size: " << args_size << std::endl;
@@ -68,12 +74,77 @@ void mprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
     std::cout << "=====================================" << std::endl;
 
     // 使用 tcp 编程, 完成 rpc 方法的远程调用.
-    int clientfd = socket(PF_INET, SOCK_STREAM, 0);
+    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == clientfd)
     {
-        std::cout << "create socket error ! errno : " << errno << std::endl;
-        exit(EXIT_FAILURE);
+        char errtxt[512] = {0};
+        sprintf(errtxt, "create socket error ! errno : %d", errno);
+        controller->SetFailed(errtxt);
+        return;
+    }
+    // 读取配置文件 rpcserver 的信息
+    std::string ip = MprpcApplication::GetInstance().GetConfig().Load("rpcserverip");
+    uint16_t port = atoi(MprpcApplication::GetInstance().GetConfig().Load("rpcserverport").c_str());
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    // 连接 rpc 服务节点
+    if (-1 == connect(clientfd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+    {
+        close(clientfd);
+        char errtxt[512] = {0};
+        sprintf(errtxt, "connect error! errno : %d", errno);
+        controller->SetFailed(errtxt);
+        return;
     }
 
+    // 发送 rpc 请求
+    if (-1 == send(clientfd, send_rpc_str.c_str(), send_rpc_str.size(), 0))
+    {
+        close(clientfd);
+        char errtxt[512] = {0};
+        sprintf(errtxt, "send  error! errno : %d", errno);
+        controller->SetFailed(errtxt);
+        return;
+    }
+
+    // 接收 rpc 请求的参数
+    char recv_buf[1024] = {0};
+    int recv_size = 0;
+    recv_size = recv(clientfd, recv_buf, 1024, 0);
+    if (recv_size == -1)
+    {
+        // 发生错误
+        perror("recv error");
+        close(clientfd);
+        return;
+    }
+    else if (recv_size == 0)
+    {
+        // 对端关闭连接
+        std::cout << "Peer has closed the connection." << std::endl;
+        close(clientfd); // 关闭本端连接
+        return;
+    }
+    else
+    {
+        // 正常接收数据
+        std::cout << "Received " << recv_size << " bytes: "
+                  << std::string(recv_buf, recv_size) << std::endl;
+    }
     
+
+// 反序列化 rpc 调用的响应数据
+if (!response->ParseFromArray(recv_buf, recv_size))
+{
+    close(clientfd);
+    char errtxt[512] = {0};
+    sprintf(errtxt, "parse error! response_str:%s", recv_buf);
+    controller->SetFailed(errtxt);
+    return;
+}
+close(clientfd);
 }
