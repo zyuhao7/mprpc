8487248990938ffa5fc8596df8e235a15727bd44
myh <zyuhao7@yeah.net>
2025-06-17 22:25:44 +0800

    mprpcchannel.cc

---
+++ src/mprpcchannel.cc
@@ -0,0 +1,7 @@
+#include "mprpcchannel.h"
+
+void MprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
+                              google::protobuf::RpcController *controller, const google::protobuf::Message *request,
+                              google::protobuf::Message *response, google::protobuf::Closure *done)
+{
+}
