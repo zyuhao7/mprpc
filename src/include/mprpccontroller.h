687a092eceac8dd3b41af0a9bf8b997ea63750ac
myh <zyuhao7@yeah.net>
2025-06-22 22:22:54 +0800

    mprpccontroller.h

---
+++ src/include/mprpccontroller.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <google/protobuf/service.h>
+#include <string>
+
+class MprpcController : public google::protobuf::RpcController
+{
+public:
+    MprpcController();
+    void Reset();
+    bool Failed() const;
+    std::string ErrorText() const;
+    void SetFailed(const std::string &reson);
+
+    // 目前未实现具体的功能
+    void StartCancel();
+    bool IsCanceled() const;
+    void NotifyOnCancel(google::protobuf::Closure *callback);
+
+private:
+    bool _failed;
+    std::string _errText;
+};
