97ebb3f5bb6b7f4d81e90e9677643e6a9abcd4d3
myh <zyuhao7@yeah.net>
2025-06-22 12:42:23 +0800

    mprpccontroller.cc

---
+++ src/mprpccontroller.cc
@@ -0,0 +1,34 @@
+#include "mprpccontroller.h"
+
+MprpcController::MprpcController()
+{
+    _failed = false;
+    _errText = "";
+}
+
+void MprpcController::Reset()
+{
+    _failed = false;
+    _errText = "";
+}
+
+bool MprpcController::Failed() const
+{
+    return _failed;
+}
+
+std::string MprpcController::ErrorText() const
+{
+    return _errText;
+}
+
+void MprpcController::SetFailed(const std::string &reason)
+{
+    _failed = true;
+    _errText = reason;
+}
+
+// 目前未实现具体的功能
+void MprpcController::StartCancel() {}
+bool MprpcController::IsCanceled() const { return false; }
+void MprpcController::NotifyOnCancel(google::protobuf::Closure *callback) {}
